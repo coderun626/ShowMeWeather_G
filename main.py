d890b5ceea8b03a094fc37d32cd3851cf8336958
coderun626 <kenesbaevdasturlew@gmail.com>
2025-04-21 04:19:27 +0500

    Webhook_Test_007

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, request
-import telegram
+import requests
 import os
 
 TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-bot = telegram.Bot(token=TOKEN)
+URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    bot.send_message(chat_id=chat_id, text="Hello from Flask!")
+    data = request.get_json()
+    chat_id = data["message"]["chat"]["id"]
+    requests.post(URL, json={
+        "chat_id": chat_id,
+        "text": "Hello from Flask!"
+    })
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host="100.20.92.101", port=5000)
+    app.run(host="0.0.0.0", port=5000)
